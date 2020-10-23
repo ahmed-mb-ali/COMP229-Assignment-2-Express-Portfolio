c800abf2e605d47d81324d055b4ffa4b2aa1638a
Ahmed Ali <hjaberty@gmail.com>
2020-10-23 14:53:53 -0400

    update package.json for heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 
 module.exports = 
 {
-    "URI" : "mongodb://localhost/business_contacts" 
+    //"URI" : "mongodb://localhost/business_contacts" 
+    "URI": "mongodb+srv://ahmed:xGkm3r7XIqPwu41D@mongodbserver.todli.mongodb.net/ahmed?retryWrites=true&w=majority"
 }
